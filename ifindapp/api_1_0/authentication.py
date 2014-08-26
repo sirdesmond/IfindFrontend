0a996360ab9663363eea32cff115153734acce48
Nayshawn Danner <dannernaytion@gmail.com>
2014-08-25 22:05:32 -0500

    Testing

--- ifindapp/api_1_0/authentication.py
+++ ifindapp/api_1_0/authentication.py
@@ -27,6 +27,7 @@ auth = HTTPBasicAuth()
 @auth.verify_password
 def verify_password(email_or_token, password):
 	print str(request.headers)
+	print type(request.headers)
 	print 'email_or_token :'+ email_or_token+password
 	print 'password:'+password
 	# print request.headers['Token']
