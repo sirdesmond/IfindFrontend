baa01f2006c4e90f776615e455efa654ca128c6b
Nayshawn Danner <dannernaytion@gmail.com>
2014-08-27 01:03:14 -0500

    Cors change

--- wsgi/api_1_0/auth.py
+++ wsgi/api_1_0/auth.py
@@ -28,6 +28,7 @@ class Auth(CORSObject):
         return make_error('Invalid username or password', 401,
                           additional_headers=added_headers) if not user.verify_password(c_password) else None
 
+    @cross_origin(origins='*', headers=['Authorization', 'Content-Type'])
     @validate_credentials(verify_password)
     def post(self):
         print 'Request Header POST:AUTH \n' + str(request.headers)
